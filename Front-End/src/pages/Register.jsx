67d311c286d1fc9112aa6d62cdb24545849deee9
SriIsainila <isainilaisainila97@gmail.com>
2026-06-30 13:25:49 +0530

    add the comand

---
+++ Front-End/src/pages/Register.jsx
@@ -0,0 +1,13 @@
+function MyForm() {
+  return (
+    <form>
+      <label>Enter your name:
+        <input type="text" />
+      </label>
+    </form>
+  )
+}
+
+createRoot(document.getElementById('root')).render(
+  <MyForm />
+);
